ad4e249dd2a57dc18a61bd18ac626589a58d3e14
Marco Concetto Rudilosso <marcocr@Marcos-MacBook-Air.local>
2022-03-25 19:10:53 +0000

    fix test

--- index.test.js
+++ index.test.js
@@ -3,11 +3,14 @@ import { pythonFunction } from "./index.js"
 const addBody = `
 def add_impl(a, b):
   return a+b
+
 def add(args):
   return add_impl(args[0], args[1])
 `
-const add = pythonFunction("add", addBody)
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(add([1,2])).toBe(3);
+
+test('adds 1 + 2 to equal 3', async () => {
+  const [add, stopWorker] = pythonFunction("add", addBody)
+  expect(await add([1,2])).toBe(3);
+  stopWorker()
 });
