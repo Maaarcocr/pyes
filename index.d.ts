c6f992a4fe3825fee7b9d8d3d17802fef3c2bf14
Marco Concetto Rudilosso <marcocr@Marcos-MacBook-Air.local>
2022-03-26 16:19:36 +0000

    add declaration file and a test case

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+type PythonFunction = (args: any) => Promise<[any, () => void]>
+export function pythonFunction(funcName: string, funcBody: string): PythonFunction;
--- index.test.js
+++ index.test.js
@@ -8,9 +8,20 @@ def add(args):
   return add_impl(args[0], args[1])
 `
 
+const testFromWil = `
+def run(a):
+  return True is False
+`
+
 
 test('adds 1 + 2 to equal 3', async () => {
   const [add, stopWorker] = pythonFunction("add", addBody)
   expect(await add([1,2])).toBe(3);
+  stopWorker()
+});
+
+test('true is not false', async () => {
+  const [run, stopWorker] = pythonFunction("run", testFromWil)
+  expect(await run({})).toBe(false);
   stopWorker()
 });
